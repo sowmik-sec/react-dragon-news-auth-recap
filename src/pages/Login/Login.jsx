7a3e0461876bbdc991f2479f5591fbdce4b24625
sowmik-sec <sowmik.sec@gmail.com>
2024-07-09 21:37:37 +0600

    create login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,63 @@
+import { Link } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 
 function Login() {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const email = form.get("email");
+    const password = form.get("password");
+    console.log(email, password);
+  };
   return (
     <div>
       <Navbar />
-      <h2 className="text-3xl">This is login</h2>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="space-y-7">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form onSubmit={handleLogin} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  name="email"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  name="password"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+            <p className="text-center">
+              Do not have an account? <Link to={"/register"}>Register</Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
