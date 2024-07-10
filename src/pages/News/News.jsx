6c80ec436df555a5f92c7f3cb0c9f54a142635f4
sowmik-sec <sowmik.sec@gmail.com>
2024-07-10 20:12:57 +0600

    Add news detail page with dynamic routing

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,5 +1,25 @@
-function News() {
-  return <div>news details</div>;
-}
+import { useParams } from "react-router-dom";
+import Header from "../Shared/Header/Header";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
+import Navbar from "../Shared/Navbar/Navbar";
+
+const News = () => {
+  const { id } = useParams();
+  return (
+    <div>
+      <Header></Header>
+      <Navbar></Navbar>
+      <div className="grid md:grid-cols-4">
+        <div className="col-span-3">
+          <h2 className="text-5xl">News Detail</h2>
+          <p>{id}</p>
+        </div>
+        <div>
+          <RightSideNav></RightSideNav>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default News;
