d4f37e391d5e7be809656e811b43a2bc27bd89a7
sowmik-sec <sowmik.sec@gmail.com>
2024-07-10 20:11:06 +0600

    Update firebase config to use environment variables

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCQfsbqGl0jxd-OpxXuk055XUhiKftHWFw",
-  authDomain: "react-dragon-news-auth-35804.firebaseapp.com",
-  projectId: "react-dragon-news-auth-35804",
-  storageBucket: "react-dragon-news-auth-35804.appspot.com",
-  messagingSenderId: "149596600857",
-  appId: "1:149596600857:web:102860f76dd08a1de20888",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
