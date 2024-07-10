4c32a295af1b29bb06041dea07fb41abf0f15baf
sowmik-sec <sowmik.sec@gmail.com>
2024-07-10 10:29:37 +0600

    firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,18 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD5t8sldRyoEzuicQC5GxlqUtopigFDrWY",
-  authDomain: "react-dragon-news-auth.firebaseapp.com",
-  projectId: "react-dragon-news-auth",
-  storageBucket: "react-dragon-news-auth.appspot.com",
-  messagingSenderId: "750044483429",
-  appId: "1:750044483429:web:77f4f5d3f33234b21f5b8c"
+  apiKey: "AIzaSyCQfsbqGl0jxd-OpxXuk055XUhiKftHWFw",
+  authDomain: "react-dragon-news-auth-35804.firebaseapp.com",
+  projectId: "react-dragon-news-auth-35804",
+  storageBucket: "react-dragon-news-auth-35804.appspot.com",
+  messagingSenderId: "149596600857",
+  appId: "1:149596600857:web:102860f76dd08a1de20888",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
+
+const auth = getAuth(app);
+
+export default auth;
