8fa92cbc0303f4f80ae7a5be133a5b94af87fbe2
sowmik-sec <sowmik.sec@gmail.com>
2024-07-10 11:04:56 +0600

    refactor: Update AuthProvider to include onAuthStateChanged
    This commit updates the AuthProvider component to include the onAuthStateChanged function from the firebase/auth library. This function allows us to listen for changes in the authentication state and update the user accordingly. The useEffect hook is used to subscribe to the auth state changes and update the user state accordingly.

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,8 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { createContext, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
@@ -9,6 +12,16 @@ function AuthProvider({ children }) {
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("user in the auth state changed ", currentUser);
+      setUser(currentUser);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
   const authInfo = { user, createUser };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
